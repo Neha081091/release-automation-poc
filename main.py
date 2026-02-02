5388a89e83e1aa98387fe724dfef582a964b7103
Neha081091 <neha.singh@deepintent.com>
2026-02-02 16:14:54 +0530

    Create main.py for release automation PoC
    Added release automation PoC with initial print statement.

---
+++ main.py
@@ -0,0 +1,27 @@
+# Release Automation PoC
+print("Starting release automation")
+```
+
+---
+
+### **Step 5: Commit/Save**
+
+Scroll down and look for a **green "Commit new file"** button.
+
+Click it!
+
+Done! ✅
+
+---
+
+## Visual Guide (If You're Confused)
+
+**It should look like:**
+```
+[Add file ▼]  ← Click here
+
+File name: main.py  ← Type this
+
+[Code area - paste code here]
+
+[Commit new file]  ← Click here
